509164d9d9af16fbeedb85e077d15c094077d25c
Ichihara Yamato <99705612+ichihara-yamato@users.noreply.github.com>
2022-05-04 21:30:48 +0900

    Newly added dart.

--- out/cpanel/run.js
+++ out/cpanel/run.js
@@ -12,7 +12,9 @@ class Code {
             // php
             'php': { 'if': true, 'switch': true, 'for': true, 'while': true, 'dowhile': true, 'function': true, 'class': true, 'get': true, 'post': true, 'json': true, 'mail': true, 'try': true, 'hello': true },
             // python
-            'py': { 'if': true, 'dictionary': true, 'with': true, 'for': true, 'while': true, 'def': true, 'class': true, 'get': true, 'post': true, 'json': true, 'try': true, 'hello': true }
+            'py': { 'if': true, 'dictionary': true, 'with': true, 'for': true, 'while': true, 'def': true, 'class': true, 'get': true, 'post': true, 'json': true, 'try': true, 'hello': true },
+            // dart
+            'dt': { 'if': true, 'switch': true, 'for': true, 'foreach': true, 'while': true, 'dowhile': true, 'function': true, 'assert': true, 'class': true, 'get': true, 'post': true, 'json': true, 'try': true, 'main': true, 'hello': true }
         };
     }
     path(type, file) {
@@ -22,6 +24,8 @@ class Code {
             case 'php':
                 return __dirname + '/code/' + type + '/' + file + '.txt';
             case 'py':
+                return __dirname + '/code/' + type + '/' + file + '.txt';
+            case 'dt':
                 return __dirname + '/code/' + type + '/' + file + '.txt';
         }
         return __dirname + '/code/js/hello.txt';
@@ -54,6 +58,13 @@ class Code {
         let code = '';
         if (fs.existsSync(this.path('py', code_type)))
             code = fs.readFileSync(this.path('py', code_type), 'utf-8');
+        return code;
+    }
+    //  Dart
+    dt(code_type) {
+        let code = '';
+        if (fs.existsSync(this.path('dt', code_type)))
+            code = fs.readFileSync(this.path('dt', code_type), 'utf-8');
         return code;
     }
 }
